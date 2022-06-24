7e313a6c3d28f12ceca470efb212ad98848f2d66
Karan Shetty <shettykaran2110@gmail.com>
2022-06-24 23:08:28 +0530

    Removed update voter function and alert positioning fixed

--- components/ui/alert.js
+++ components/ui/alert.js
@@ -5,7 +5,7 @@ const Alert = ({ children, isError = false }) => {
     <div
       className={`${
         isError ? 'bg-red-100 text-red-700' : 'bg-green-100 text-green-700'
-      } rounded-lg py-5 px-6 mb-4 text-base absolute bottom-4 right-4 z-50`}
+      } rounded-lg py-5 px-6 mb-4 text-base fixed bottom-4 right-4 z-50`}
       role="alert"
     >
       {children}
--- routes/voter.js
+++ routes/voter.js
@@ -4,7 +4,6 @@ const {
   create,
   authenticate,
   getAll,
-  updateById,
   deleteById,
   resultMail,
 } = require('../controllers/voter')
@@ -14,7 +13,6 @@ const router = express.Router()
 router.post('/register', create)
 router.post('/authenticate', authenticate)
 router.post('/', getAll)
-router.put('/:voterId', updateById)
 router.delete('/:voterId', deleteById)
 router.post('/resultMail', resultMail)
 
